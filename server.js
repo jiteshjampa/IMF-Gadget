6413c2f17083f5c70e19a096afd7e8191874cbb2
JiteshReddy26 <jitesh@wellness360.co>
2025-01-25 17:03:29 +0530

    adding app.listen to add in deployement in railway

--- server.js
+++ server.js
@@ -53,6 +53,10 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 // Graceful Shutdown
 process.on("SIGINT", async () => {
